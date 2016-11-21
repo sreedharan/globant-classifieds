6e91462f3516425cf1508166dfa680c1badead08
sreedharanp <s.namboodiry@globant.com>
2016-11-21 11:44:19 +0530

    app.js.mod

--- js/script.js
+++ js/script.js
@@ -12,3 +12,50 @@ app.controller("headerController", ['$scope', function($scope) {
         isFirstDisabled: false
     };
 }]);
+
+
+app.controller("landingController", ['$scope', function($scope) {
+    $scope.showData = function() {
+        $scope.datalists = [
+            { "name": "Housing", "color": "blue" },
+            { "name": "Automotive", "color": "green" },
+            { "name": "Furniture", "color": "orange" },
+            { "name": "Electronics", "color": "black" },
+            { "name": "Household", "color": "blue" },
+            { "name": "Other", "color": "green" },
+            { "name": "Housing", "color": "blue" },
+            { "name": "Automotive", "color": "green" },
+            { "name": "Furniture", "color": "orange" },
+            { "name": "Electronics", "color": "black" },
+            { "name": "Household", "color": "blue" },
+            { "name": "Other", "color": "green" },
+            { "name": "Housing", "color": "blue" },
+            { "name": "Automotive", "color": "green" },
+            { "name": "Furniture", "color": "orange" },
+            { "name": "Electronics", "color": "black" },
+            { "name": "Household", "color": "blue" },
+            { "name": "Other", "color": "green" },
+            { "name": "Housing", "color": "blue" },
+            { "name": "Automotive", "color": "green" },
+            { "name": "Furniture", "color": "orange" },
+            { "name": "Electronics", "color": "black" },
+            { "name": "Household", "color": "blue" },
+            { "name": "Other", "color": "green" }
+        ]
+
+        var pagesShown = 2;
+        var pageSize = 3;
+
+        $scope.paginationLimit = function(data) {
+            return pageSize * pagesShown;
+        };
+        $scope.hasMoreItemsToShow = function() {
+            return pagesShown < ($scope.datalists.length / pageSize);
+        };
+        $scope.showMoreItems = function() {
+            pagesShown = pagesShown + 2;
+        };
+
+
+    }
+}]);
